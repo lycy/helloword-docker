3c03ed6666e233f2eb929940214608afb55d6165
luyeciyu <weisen.yang@hand-china.com>
2017-12-25 11:48:57 +0800

    ADD: new

---
+++ src/main/java/mydocker/HellowordApplication.java
@@ -0,0 +1,12 @@
+package mydocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowordApplication.class, args);
+	}
+}
---
+++ src/main/java/mydocker/test/HelloWordController.java
@@ -0,0 +1,27 @@
+package mydocker.test;
+
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author weisen.yang@hand-china.com
+ * @Date 2017/12/25 10:35
+ * @name
+ * @description
+ */
+@RestController
+@RequestMapping("test/")
+public class HelloWordController {
+
+    @RequestMapping(value = "/helloWord")
+    public ResponseEntity helloWord(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("param", "Hello Word!");
+        return ResponseEntity.ok(map);
+    }
+}
